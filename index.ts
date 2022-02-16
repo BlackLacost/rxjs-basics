6e5a22516d2f769e7efb3176a6cbe07546c302af
BlackLacost <blacklacost@gmail.com>
2022-02-16 21:25:12 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -4,44 +4,42 @@ console.clear();
 // begin lesson code
 import { Observable } from 'rxjs';
 
-/*
- * Observers can register up to 3 callbacks
- * next is called 1:M times to push new values to observer
- * error is called at most 1 time should an error occur
- * complete is called at most 1 time on completion.
- */
 const observer = {
-    next: value => console.log('next', value),
-    error: error => console.log('error', error),
-    complete: () => console.log('complete!')
+  next: (value) => console.log('next', value),
+  error: (error) => console.log('error', error),
+  complete: () => console.log('complete!'),
 };
 
-const observable = new Observable(subscriber => {
-    subscriber.next('Hello');
-    subscriber.next('World');
-    /*
-     * Once complete is called, observable will be cleaned up
-     * and no future values delivered.
-     */
+const observable = new Observable((subscriber) => {
+  let count = 0;
+  // Observables can deliver 0:M values synchronous or asynchronously
+  const id = setInterval(() => {
+    subscriber.next(count);
+    // calling complete also invokes the cleanup function you return
     subscriber.complete();
-    /*
-     * These values will not be logged as the observable
-     * has already completed.
-     */
-    subscriber.next('Hello');
-    subscriber.next('World');
+    count += 1;
+  }, 1000);
+
+  /*
+   * You can return a function to clean up any resources that were
+   * created with subscription. In this case, we need to clear
+   * the active interval. When using RxJS's built in creation operators
+   * this will be handled for us.
+   */
+  return () => {
+    console.log('called');
+    clearInterval(id);
+  };
 });
 
-/* 
- * Subscribe hooks observer up to observable, beginning execution.
- * This creates a 1 to 1 relationship between the producer
- * (observable) and the consumer (observer).
- */
+// adding logs to show observable emitting asynchronously
+console.log('before');
 observable.subscribe(observer);
+console.log('after');
 
 /********************
  * Have a question, comment, or just want to chat about RxJS?
- * Ping me on Ultimate Courses slack or on 
+ * Ping me on Ultimate Courses slack or on
  * Twitter https://twitter.com/btroncone
  * I look forward to hearing from you!
  * For additional RxJS info and operator examples check out
